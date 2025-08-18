53841c710c921507f997d969ad3ae3d65abd0df0
Md. Fahim Alam <kfahim2280@gmail.com>
2025-08-18 23:39:42 +0600

    project created

---
+++ Workflow & Document Management System/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Workflow___Document_Management_System.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
